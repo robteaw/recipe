7d886bc93a22d851d47bd5e24ee9b9e5530ce402
Robert Tea <robteaw@gmail.com>
2024-05-01 00:13:14 -0500

    Sass error fixed and Home path added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // npm i react-router-dom
+import Home from "./pages/Home/index";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
