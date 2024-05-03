b48e20dd2c3bcd794c1140ed5e42c6cf62a1817e
Robert Tea <robteaw@gmail.com>
2024-05-02 23:54:21 -0500

    Navbar and styling added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // npm i react-router-dom
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/index";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
         </Routes>
