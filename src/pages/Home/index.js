657d056dcaa247eb1418ae6199c5658cb5b06bc1
Robert Tea <robteaw@gmail.com>
2024-07-24 22:55:32 -0500

    PickCard component  added to Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,12 @@
 import React from "react";
 import "./index.scss";
-import jamcake from "../../assets/images/jamcake.jpg";
-import friedrice from "../../assets/images/friedrice.jpg";
-import tortillasoup from "../../assets/images/tortillasoup.jpg";
+import PickCard from "../../components/PickCard/PickCard";
 
 function index() {
   return (
     <div className="home">
       <h2>Top Picks</h2>
-      <div className="imgSet">
-        <div className="imgContainer">
-          <img src={jamcake} alt="" className="" />
-          <h4>Strawberry Jam Coffee Cake</h4>
-        </div>
-        <div className="imgContainer">
-          <img src={friedrice} alt="" className="" />
-          <h4>Fried Rice</h4>
-        </div>
-        <div className="imgContainer">
-          <img src={tortillasoup} alt="" className="" />
-          <h4>Mexican Tortilla Chicken Soup</h4>
-        </div>
-      </div>
+      <PickCard />
     </div>
   );
 }
